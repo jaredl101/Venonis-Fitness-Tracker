cf852054865bd85010cbb1f8408cbbe3e57aa2e9
Jared Lidenberg <jaredl101@hotmail.com>
2020-07-12 00:45:34 -0500

    Updated profile, added username/date/details

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -9,11 +11,20 @@ class Profile extends Component {
 render(){
   return (
     <div>
-
+      <img src={this.props.user.avatar} alt="avatar" />
+      <p>Name: {this.props.user.first_name} {this.props.user.last_name}</p>
+      <p>Username: {this.props.user.username}</p>
+      <p>Account Created: {moment(this.props.user.date_created).subtract(10, 'days').calendar()}</p>
     </div>
   )
 }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    exercise: state.exercise,
+  }
+}
 
-export default Profile;
+export default connect(mapStateToProps)(Profile);
